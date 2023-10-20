760830863a40802d5356da0990b0c93c863d1dfe
Mathius <mathiuskormasela12@gmail.com>
2023-10-21 00:13:48 +0700

    Learning Hello World (First Day)

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+/*
+    This function will be called
+    immediatelly when this file
+    is executed.
+
+    Kotlin don't need us to write
+    semi-colon at the end of code.
+    So similar with Javascript
+ */
+fun main() {
+    // To print text to our console (Multiline)
+    // Similar with System.out.println in Java
+    println("Hello World");
+
+    // To print text to our console (Single Line)
+    // Similar with System.out.print in Java
+    print("Mathius")
+    print(" ")
+    print("Kormasela")
+}
